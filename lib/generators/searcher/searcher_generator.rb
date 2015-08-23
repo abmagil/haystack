ec47f77729abae2fe19568aee0ad860a5fd6f917
Aaron Magil <aaron@societyofgrownups.com>
2015-08-23 11:21:57 -0400

    Checks command line option against app classes
    When creating a searcher, if the class passed does not exist, haystack
    will output a warning and continue to the next given target class
    
    Fixes #2

--- lib/generators/searcher/searcher_generator.rb
+++ lib/generators/searcher/searcher_generator.rb
@@ -4,7 +4,6 @@ class SearcherGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
 
   def create_searcher_model
-    
     dir_name = File.join("app", "searchers")
     # unless Dir.exists? guards against deleting entire directory on `rails destroy`
     empty_directory dir_name unless Dir.exists?(dir_name)
@@ -16,6 +15,16 @@ class SearcherGenerator < Rails::Generators::NamedBase
 
   private
   def model_names
-    ARGV.map(&:underscore)
+    ARGV
+      .select{|model_name| is_app_class?(model_name)}
+      .map(&:underscore)
+  end
+
+  # Filter out anything from the command line which is not a class in the app
+  def is_app_class?(model_name)
+    model_name.constantize
+  rescue NameError
+    say set_color("     warning", :yellow, true) + "  #{model_name} is an unknown class"
+    false
   end
 end
